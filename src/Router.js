5d5aca3771af07701c6c75522869a0921313a267
Bong <2021bongwonny@gmail.com>
2022-11-29 15:16:52 +0900

    🩹Modify : 재배포를 위한 수정 - homepage 및 name 수정 - 브라우저 라우터 해시 라우터로 변경 - PR 템플릿 삭제

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import RecordMain from './pages/record/RecordMain';
 import AudioPlayer from './pages/audio/AudioPlayer';
 import { ThemeProvider } from 'styled-components';
@@ -7,12 +7,12 @@ import * as Theme from './styles/Theme';
 const Router = (props) => {
   return (
     <ThemeProvider theme={Theme}>
-      <BrowserRouter basename='haii-audio-player-8team'>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<RecordMain />} />
           <Route path='/audio' element={<AudioPlayer />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 };
